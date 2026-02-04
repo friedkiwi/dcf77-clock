0f1867ed299ee14631765352d2dbe0c325873911
Yvan Janssens <yvanj@cyber.gent>
2026-02-04 13:02:14 +0100

    Implement wifi configuration

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <DCF77FreeRTOS.h>
 #include <TFT_eSPI.h>
+#include <WiFi.h>
+#include <WiFiManager.h>
 
 
 // Create DCF77 receiver instance
@@ -13,6 +15,7 @@ TFT_eSPI display = TFT_eSPI(TFT_WIDTH, TFT_HEIGHT);
 // Global variables:
 int uptimeSeconds = 0;
 int syncedShown = 0;
+WiFiManager wifiManager;
 
 // day of week string array:
 constexpr const char* kDayNames[] = {
@@ -20,6 +23,15 @@ constexpr const char* kDayNames[] = {
   "Thursday", "Friday", "Saturday", "Sunday"
 };
 
+bool buttonHeldLow(uint32_t holdMs) {
+  if (digitalRead(WIFI_RESET_BTN) != LOW) return false; // active-low button
+  uint32_t start = millis();
+  while (digitalRead(WIFI_RESET_BTN) == LOW) {
+    if (millis() - start >= holdMs) return true;
+    delay(10);
+  }
+  return false;
+}
 
 void setup() {
   Serial.begin(115200);
@@ -27,6 +39,9 @@ void setup() {
 
   Serial.println("[FW] DCF77-Clock V0.1 --- https://github.com/friedkiwi/dcf77-clock");
   
+  // configure wifi reset button GPIO:
+  pinMode(WIFI_RESET_BTN, INPUT);
+
   // enable display backlight
   pinMode(TFT_BL, OUTPUT);
   digitalWrite(TFT_BL, HIGH);
@@ -42,6 +57,35 @@ void setup() {
 
   receiver.begin(-1);  // optional LED pin
   Serial.println("[DCF77] DCF77 receiver initialised.");
+
+  // init wifi:
+
+  Serial.println("[WIFI] Connecting to WiFi...");
+
+  WiFi.enableIpV6();
+  WiFi.setHostname("DCF77-Clock");
+
+  display.setTextColor(TFT_WHITE);
+  display.setTextSize(2);
+  display.setCursor(0,0);
+  if (wifiManager.getWiFiIsSaved() == true) {
+    display.print("Connecting to saved\nWiFi network...");
+  } else {
+    display.print("Please set up wifi using the DCF77-Clock-Setup access point.");
+  }
+  
+  
+  bool ok = wifiManager.autoConnect("DCF77-Clock-Setup");
+  if (!ok) {
+    Serial.println("[WIFI] Failed to connect and hit timeout");
+  } else {
+    Serial.println("[WIFI] Connected to WiFi.");
+    Serial.print("[WIFI] IP address: ");
+    Serial.println(WiFi.localIP());
+    Serial.print("[WIFI] IPv6 address: ");
+    Serial.println(WiFi.localIPv6());
+  }
+  display.fillScreen(TFT_BLACK);
 }
 
 
@@ -110,6 +154,14 @@ void loop() {
   if (uptimeSeconds > 32000) uptimeSeconds = 0;
 
   uptimeSeconds++;
+
+  if (buttonHeldLow(1000)) {
+    Serial.println("[WIFI] Button held 1s, clearing saved Wi-Fi settings and restarting...");
+    wifiManager.resetSettings();
+    delay(200);
+    ESP.restart();
+  }
+
 
   
   delay(1000);
